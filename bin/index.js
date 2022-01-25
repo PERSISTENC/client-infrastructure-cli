20112e55bf2ba7c3415b709f118f6f035455aad5
王思渊 <31314554@qq.com>
2022-01-25 11:06:47 +0800

    :smile:

--- bin/index.js
+++ bin/index.js
@@ -7,15 +7,12 @@ program
     .usage('<command> [options]')
 
     //  This is create project name
-program
-    .command('create <project-name>')
-    .description('create a new project')
-    .action((name, cmd) => {
-        if (minimist(process.argv.slice(3))._.length > 1) {
-          console.log(chalk.yellow('\n Info: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.'))
-        }
-        require('../lib/create')(name)
-      })
+program.command('create <project-name>').description('create a new project').action((name, cmd) => {
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(chalk.yellow('\n Info: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.'))
+    }
+    require('../lib/create')(name)
+})
 
 // 解析命令行参数
 program.parse(process.argv)
